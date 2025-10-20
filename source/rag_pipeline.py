f4491b8658acb3baa957da336a0ae52250d888af
ed-tadini <edoardotadini5@gmail.com>
2025-10-20 17:14:40 +0200

    update pipeline

--- source/rag_pipeline.py
+++ source/rag_pipeline.py
@@ -3,6 +3,7 @@ from langchain_community.vectorstores import VectorStore
 from enhancer import Enhancer
 from critic import Critic
 from dotenv import load_dotenv
+import time
 
 
 load_dotenv()
@@ -14,21 +15,18 @@ class Pipeline:
         self.vector_store = VectorStore(persist_directory = vectorstore_path)
         self.vector_store.load_vectorstore()
 
-    def retrieve_chuncks(self, enhancer_context: dict, user_query: str = '', 
-                    initial_num: int = 20, max_attempts: int = 2, 
+    
+    def refine_retrieval(self, enhancer_context: dict, initial_chuncks: list[tuple[any, float]], 
+                    user_query: str = '', initial_num: int = 20, max_attempts: int = 2, 
                     confidence_threshold: float = 0.7):
         
         if user_query == '':
-            raise ValueError('no user query recieved by retrival method')
+            raise ValueError('no user query recieved by refine retrival method')
 
-        # Step 1: raw semantic retrieval
-        initial_chunks = self.vector_store.search(user_query, k=initial_num)
-        
-        # Step 2: critic review and feedback loop
         for attempt in range(max_attempts):
             critic = Critic(
                 enhancer_message=enhancer_context,
-                chunks=initial_chunks
+                chunks=initial_chuncks
             )
             filtered_chunks, critic_metadata = critic.execute()
             
@@ -38,6 +36,33 @@ class Pipeline:
             
             # Low confidence: get more chunks
             more_chunks = self.vector_store.search(user_query, k=10)
-            initial_chunks.extend(more_chunks)
+            initial_chuncks.extend(more_chunks)
 
         return filtered_chunks
+    
+    def execute(self, question: str = ''):
+        
+        if question == '':
+            raise ValueError('no question received by the pipeline')
+        
+        start_time = time.time()
+
+        # Step 1: enhance the user query
+        enhancer = Enhancer()
+        enhancer_message = enhancer.enhance(question)
+
+        # Step 2: first retrieval
+        CHAR_THRESHOLD = 50
+        
+        if len(question) < CHAR_THRESHOLD:
+            user_query = ' '.join(enhancer_message['for_retrieval']) + question.lower()
+        else:
+            user_query = question.lower()
+
+        INITIAL_CHUNCK_NUM = 20
+
+        initial_chuncks = self.vector_store.similarity_search_with_relevance_scores(
+            user_query,
+            k = INITIAL_CHUNCK_NUM
+        )
+
